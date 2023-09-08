3a9863a01ff14a0c6481698878b9b0e74300e734
Klemen Kobau <klemen.kobau@gmail.com>
2023-09-08 21:19:40 +0200

    feat: created rust workspace

--- video-server/src/video.rs
+++ video-server/src/video.rs
@@ -1,6 +1,10 @@
 use tokio::fs;
 
-use crate::{config::Config, errors::AppError, dto::video::{Video, VideoSegment}};
+use crate::{
+    config::Config,
+    dto::video::{Video, VideoSegment},
+    errors::AppError,
+};
 
 pub async fn read_videos(config: &Config) -> Result<Vec<Video>, AppError> {
     let video_folder_path = config.video_config().video_folder_path();
@@ -9,15 +13,26 @@ pub async fn read_videos(config: &Config) -> Result<Vec<Video>, AppError> {
 
     let content = fs::read(file_path).await?;
     let parsed = String::from_utf8(content)?;
-    let videos: Vec<_> = parsed.split("\n")
-        .map(|x| x.split_once(";").expect("The videos should be in the correct format."))
-        .map(|(name, uuid)| Video{name: name.to_owned(), uuid: uuid.to_owned()})
+    let videos: Vec<_> = parsed
+        .split("\n")
+        .map(|x| {
+            x.split_once(";")
+                .expect("The videos should be in the correct format.")
+        })
+        .map(|(name, uuid)| Video {
+            name: name.to_owned(),
+            uuid: uuid.to_owned(),
+        })
         .collect();
 
     Ok(videos)
 }
 
-pub async fn read_segment(config: &Config, video_id: String, segment_number: String) -> Result<VideoSegment, AppError> {
+pub async fn read_segment(
+    config: &Config,
+    video_id: String,
+    segment_number: String,
+) -> Result<VideoSegment, AppError> {
     let video_folder_path = config.video_config().video_folder_path();
     let file_path = format!("{}/{}/{}", video_folder_path, video_id, segment_number);
 
