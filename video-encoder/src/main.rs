2f370f19eccc76bb69ed813e13b5edb1f3eea126
Klemen Kobau <klemen.kobau@gmail.com>
2023-09-09 13:37:29 +0200

    feat: api file

--- video-encoder/src/main.rs
+++ video-encoder/src/main.rs
@@ -1,8 +1,11 @@
 use errors::AppError;
 
+mod api;
 mod errors;
 
 #[tokio::main]
 async fn main() -> Result<(), AppError> {
+    // ffmpeg -i video-world-war-2.mp4 -codec: copy -start_number 0 -hls_time 7 -hls_list_size 0 -f hls -hls_segment_filename output/filename%04d.hls filename.m3u8
+
     Ok(())
 }
