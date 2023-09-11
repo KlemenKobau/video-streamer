9c5ed53a1c1ca9e535fe8c73a8f83fc7036e785d
Klemen Kobau <klemen.kobau@gmail.com>
2023-09-11 17:01:41 +0200

    feat: sun scripts

---
+++ video-encoder/run.sh
@@ -0,0 +1,6 @@
+export HOSTNAME=localhost
+export PORT=8080
+export VIDEO_FOLDER_PATH=../videos
+export TEMP_VIDEO_FOLDER=../temp-videos
+
+cargo run
---
+++ video-server/run.sh
@@ -0,0 +1,5 @@
+export HOSTNAME=localhost
+export PORT=8081
+export VIDEO_FOLDER_PATH=../videos
+
+cargo run
